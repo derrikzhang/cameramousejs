144ec9ae8ab8c4de932511f0f27626170afa2088
derrikzhang <derrikzhang@gmail.com>
2024-05-03 13:44:20 -0400

    profile

--- src/pyTracker/src/main.py
+++ src/pyTracker/src/main.py
@@ -6,33 +6,60 @@ import cv2
 import os
 import sys
 
+import cProfile
+import re
+import pstats
+
+
+
 if __name__ == "__main__":
 
+    profiler = cProfile.Profile()
 
-    frameSize = getFrameSize()
-    trackerState.setWebcamFrameSize(frameSize[0], frameSize[1])
-    count = 0
-    while True:
-        face, pose, pos, guesture, face_confidence, gesture_confidences = trackFace(trackerState)
-        sendTrackingInfo(face, frameSize, pose, pos, guesture, face_confidence, gesture_confidences)
-        convertFaceTrackingToMouseMovement(face, frameSize, pose, pos, guesture, trackerState)
-        
-        # get config every now and then
-        if count % 20 == 0:
-            getLatestAppSettingsFromServer(trackerState)
+    cap = cv2.VideoCapture(0)
+    prev_time = 0
 
-        # Stop if escape key is pressed
-        k = cv2.waitKey(30) & 0xff
-        if k == 27:
-            break
+    try:
 
-        # On Windows, close both windows if the close button is pressed
-        if sys.platform == 'win32':
-            if cv2.getWindowProperty('Face Tracker', cv2.WND_PROP_VISIBLE) < 1:        
+        frameSize = getFrameSize()
+        trackerState.setWebcamFrameSize(frameSize[0], frameSize[1])
+        count = 0
+        while True:
+
+            context = globals().copy()  # Start with a copy of globals
+            context.update(locals())    # Update with all local variables
+            
+            face, pose, pos, guesture, face_confidence, gesture_confidences = trackFace(trackerState)
+            
+            profiler.enable()
+            sendTrackingInfo(face, frameSize, pose, pos, guesture, face_confidence, gesture_confidences)
+            convertFaceTrackingToMouseMovement(face, frameSize, pose, pos, guesture, trackerState)
+            profiler.disable()
+
+
+            # get config every now and then
+            if count % 20 == 0:
+                getLatestAppSettingsFromServer(trackerState)
+
+            # Stop if escape key is pressed
+            k = cv2.waitKey(30) & 0xff
+            if k == 27:
                 break
 
-        cv2.setWindowProperty('Face Tracker', cv2.WND_PROP_TOPMOST, 1)
-        # reset to prevent overflow
-        count = 0 if count > 100 else count + 1
+            # On Windows, close both windows if the close button is pressed
+            if sys.platform == 'win32':
+                if cv2.getWindowProperty('Face Tracker', cv2.WND_PROP_VISIBLE) < 1:        
+                    break
 
+            cv2.setWindowProperty('Face Tracker', cv2.WND_PROP_TOPMOST, 1)
+            # reset to prevent overflow
+            count = 0 if count > 100 else count + 1
+
+
+    finally:
+        profiler.disable()
+        with open('profile_output.txt', 'w') as f:
+            s = pstats.Stats(profiler, stream=f)
+            s.strip_dirs().sort_stats('time').print_stats()
+    cap.release()
     cv2.destroyAllWindows()
